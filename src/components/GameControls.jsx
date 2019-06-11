6c9a3fa21dfec9e7a047e0321174a5ab5e1318a6
Bundit Hongmanee <bundit.j.h@gmail.com>
2019-06-11 14:13:29 -0700

    first commits

---
+++ src/components/GameControls.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class GameControls extends React.Component {
+  render() {
+    return (
+
+    );
+  }
+
+  handleClick(i) {
+
+  }
+
+  undo() {
+
+  }
+}
+
+export default connect()(GameControls);
