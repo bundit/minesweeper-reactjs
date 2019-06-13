e0b693620d8746d173adf935957eb479dc34ece2
Bundit Hongmanee <bundit.j.h@gmail.com>
2019-06-13 12:07:03 -0700

    updated to update styling when revealed

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -3,13 +3,14 @@ import { connect } from 'react-redux';
 import styles from '../css-modules/Square.module.css'
 
 function Square(props) {
+  let value = props.state.value ? props.state.value : null;
   return (
     <button
-      className={styles.square}
+      className={!props.state.revealed && styles.square}
       onClick={() => props.handleClick(props.index)}
       onContextMenu={e => { e.preventDefault(); props.handleFlag(props.index);}}
     >
-      {props.state.revealed && props.state.value}
+      {props.state.revealed && !props.state.flagged && value}
     </button>
   );
 }
