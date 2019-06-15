995a60f03339cd0857306837537edf04f717ae8c
Bundit Hongmanee <bundit.j.h@gmail.com>
2019-06-15 15:40:28 -0700

    added cell size and changed board sizes

--- src/constants/GameConstants.js
+++ src/constants/GameConstants.js
@@ -1,10 +1,11 @@
 // Board Dimensions
 export const EASY_ROWS = 10;
-export const EASY_COLUMNS = 8;
-export const MEDIUM_ROWS = 14;
-export const MEDIUM_COLUMNS = 12;
-export const HARD_ROWS = 14;
-export const HARD_COLUMNS = 18;
+export const EASY_COLUMNS = 9;
+export const MEDIUM_ROWS = 13;
+export const MEDIUM_COLUMNS = 11;
+export const HARD_ROWS = 17;
+export const HARD_COLUMNS = 15;
+export const CELL_SIZE = 25;
 
 // Mines
 export const EASY_MINES = 10;
