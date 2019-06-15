ef69976ab9f497c0384ca98c223948aa3ab6ad15
Bundit Hongmanee <bundit.j.h@gmail.com>
2019-06-15 15:39:29 -0700

    updated square and styles

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -3,23 +3,26 @@ import { connect } from 'react-redux';
 import styles from '../css-modules/Square.module.css'
 
 function Square(props) {
-  let value;
+  let valueToDisplay;
   const thisSquare = props.state;
+  if (!thisSquare) return <button></button>;
 
-  if (thisSquare.isRevealed) {
-    value = thisSquare.value ? thisSquare.value : null;
+  if (thisSquare && thisSquare.isRevealed) {
+    valueToDisplay = thisSquare.value ? thisSquare.value : null;
   }
-  else if (thisSquare.isFlagged) {
-    value = <img className={styles.flag} src="/favicon.ico" alt="flag"/>
+  else if (thisSquare && thisSquare.isFlagged) {
+    valueToDisplay = <img className={styles.flag} src="/favicon.ico" alt="flag"/>
   }
+
+  const squareStyle = props.index % 2 ? styles.oddSquare : styles.evenSquare;
 
   return (
     <button
-      className={!thisSquare.isRevealed ? styles.square : styles.revealedSquare}
+      className={!thisSquare.isRevealed ? squareStyle : styles.revealedSquare}
       onClick={() => props.handleClick(props.index)}
       onContextMenu={e => { e.preventDefault(); props.handleFlag(props.index);}}
     >
-      {value === 'b' ? <i className="fa fa-bomb"/> : value}
+      {valueToDisplay === 'b' ? <i className="fa fa-bomb"/> : valueToDisplay}
     </button>
   );
 }
