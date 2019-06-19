2052c522038f1954639efe96f76a092670a449b3
Bundit Hongmanee <bundit.j.h@gmail.com>
2019-06-19 15:36:00 -0700

    added chart styling

--- src/components/HighScoreChart.jsx
+++ src/components/HighScoreChart.jsx
@@ -2,18 +2,23 @@ import React from 'react';
 import styles from '../css-modules/HighScoreChart.module.css';
 
 function HighScoreChart(props) {
+  // fetch here
+
   return (
     <div className={styles.chartWrapper} style={{display: props.showGame && 'none'}}>
       <div className={styles.chart}>
         <h3>Easy</h3>
         <ol>
+          <li><p>1. name</p> <p>10 seconds</p> </li>
         </ol>
       </div>
+      <div className="clear"/>
       <div className={styles.chart}>
         <h3>Medium</h3>
         <ol>
         </ol>
       </div>
+      <div className="clear"/>
       <div className={styles.chart}>
         <h3>Hard</h3>
         <ol>
