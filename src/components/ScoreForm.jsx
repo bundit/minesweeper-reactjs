4df89ebe312c2901954d2a2639cd17c4e11a93d9
Bundit Hongmanee <bundit.j.h@gmail.com>
2019-06-25 15:57:09 -0700

    added score form css modules

--- src/components/ScoreForm.jsx
+++ src/components/ScoreForm.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import propTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addHighscore } from '../actions/highscoreActions';
+import styles from '../css-modules/ScoreForm.module.css';
 
 class ScoreForm extends React.Component {
   constructor(props) {
@@ -13,15 +14,20 @@ class ScoreForm extends React.Component {
 
   render() {
     return (
-      <div className="formWrapper" style={{display: !this.props.showForm && 'none'}}>
+      <div className={styles.formWrapper} style={{display: !this.props.showForm && 'none'}}>
         <form>
-          <label>Enter your name!</label><br/>
-          <input
-            type="text"
-            name="username"
-            value={this.state.username}
-          /> <br/>
-          <button type="submit">Submit</button>
+          <div className={styles.formTitleWrapper}>
+            <h4>Add Your Highscore!</h4>
+          </div>
+          <div className={styles.form}>
+            <label>Name:</label><br/>
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+            /> <br/>
+            <button type="submit">Submit</button>
+          </div>
         </form>
       </div>
     )
