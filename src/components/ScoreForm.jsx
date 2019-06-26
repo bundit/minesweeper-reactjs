eef3610b98dd1e89297a02aa4dcee7332dcf3a1e
Bundit Hongmanee <bundit.j.h@gmail.com>
2019-06-26 12:47:30 -0700

    added onchange and onsubmit handlers

--- src/components/ScoreForm.jsx
+++ src/components/ScoreForm.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import propTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addHighscore } from '../actions/highscoreActions';
+import { TOGGLE_SHOW_FORM, CONFIGURE_NEW_BOARD } from '../actions/types';
+// CSS
 import styles from '../css-modules/ScoreForm.module.css';
+
 
 class ScoreForm extends React.Component {
   constructor(props) {
@@ -10,12 +13,35 @@ class ScoreForm extends React.Component {
     this.state = {
       username: '',
     }
-  }
 
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  onSubmit(e) {
+    e.preventDefault();
+
+    const scorePost = {
+      username: this.state.username,
+      time: this.props.time,
+      mode: this.props.mode
+    }
+
+    this.props.addHighscore(scorePost);
+    this.props.dispatch({type: TOGGLE_SHOW_FORM});
+    this.props.dispatch({type: CONFIGURE_NEW_BOARD});
+  }
+  handleCancel() {
+    this.props.dispatch({type: TOGGLE_SHOW_FORM});
+  }
   render() {
     return (
       <div className={styles.formWrapper} style={{display: !this.props.showForm && 'none'}}>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <div className={styles.formTitleWrapper}>
             <h4>Add Your Highscore!</h4>
           </div>
@@ -24,9 +50,13 @@ class ScoreForm extends React.Component {
             <input
               type="text"
               name="username"
+              onChange={this.onChange}
               value={this.state.username}
             /> <br/>
-            <button type="submit">Submit</button>
+            <div>
+              <button onClick={this.handleCancel}> Cancel</button>
+              <button type="submit">Submit</button>
+            </div>
           </div>
         </form>
       </div>
@@ -34,4 +64,8 @@ class ScoreForm extends React.Component {
   }
 }
 
-export default ScoreForm;
+ScoreForm.propTypes = {
+  addHighscore: propTypes.func.isRequired,
+}
+
+export default connect(null, { addHighscore })(ScoreForm);
